ce8f9f0ce09d0686169ca29af713fd7304c72d32
Om Kumar Kaushik <omkumar.kaushik@gmail>
2024-04-28 17:13:43 +0530

    new test case with default constructor fix

--- src/test/java/com/example/rentalproperty/Demo2ApplicationTests.java
+++ src/test/java/com/example/rentalproperty/Demo2ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.example.rentalproperty;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.stream.Collectors;
@@ -29,6 +31,9 @@ class Demo2ApplicationTests {
 	@Autowired
 	private IRentalPropertyService service;
 	
+	/*
+	 * Isse mockbean banate hai to avoid hitting db again and again
+	 */
 	@MockBean
 	private IRentalPropertyRepository repository;
 	
@@ -48,12 +53,37 @@ class Demo2ApplicationTests {
 		assertEquals(1, service.getById(1).size());
 	}
 	
-//	@Test
-//	public void addPropertyTest() {
-//		RentalPropertyModel rpModel = new RentalPropertyModel(1,"Ray-ban","resden","3BHK","Marathalli"
-//						,"Benagaluru urban","Bengalore",2000.0,105.0);
+	/*
+	 * Void method ke liye Verify use hota hai to check if 
+	 * repository is hit at least one time.
+	 */
+	@Test
+	public void addPropertyTest() {
+		RentalPropertyModel rpModel = new RentalPropertyModel(1,"Ray-ban","resden","3BHK","Marathalli"
+						,"Benagaluru urban","Bengalore",2000.0,105.0);
 //		when(repository.addRentalProperty(rpModel)).thenReturn(rpModel);
 //		assertEquals(rpModel,service.addRentalProperty(rpModel));
-//	}
+		service.addRentalProperty(rpModel);
+		verify(repository, times(1)).addRentalProperty(rpModel);
+	}
+	
+	@Test
+	public void deletePropertyTest() {
+		Integer id = 21;
+		service.deleteRentalProperty(id);
+		verify(repository,times(1)).deleteRentalProperty(id);
+	}
+	
+	@Test
+	public void getByCityTest() {
+		when(repository.findByCity("Delhi")).thenReturn(Stream.
+				of(new RentalPropertyModel(1,"Ray-ban","resden","3BHK","Marathalli"
+						,"Benagaluru urban","Delhi",2000.0,105.0),
+						new RentalPropertyModel(2,"Ray-ban","resden","3BHK","Marathalli"
+								,"Benagaluru urban","Delhi",2000.0,105.0),
+						new RentalPropertyModel(1,"Ray-ban","resden","3BHK","Marathalli"
+								,"Benagaluru urban","Delhi",2000.0,105.0)).collect(Collectors.toList()));
+		assertEquals(3, service.getByCity("Delhi").size());
+	}
 
 }
